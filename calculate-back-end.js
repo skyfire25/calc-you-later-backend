422a7f8c54f929e214d0a1a742d9ab73f1dd03e9
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-23 11:05:34 -0400

    changes

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,29 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
-
- 
+function calculate(num1, num2, op) {
+  num1= Number(num1)
+  num2= Number(num2)
+  if (op=== '+' || op === 'plus' || op=== 'added to') {
+    return num1 + num2
+  }else if(op === '-' || op === 'minus' || op=== 'subtracted from'){
+    return num1 - num2
+  
+  }else if(op=== 'x' || op=== 'times'|| op=== 'X' || op==='multiplied by') {
+    return num1 * num2
+  
+  }else if(op=== '/' || op==='divided by') {
+    return num1 / num2
+  
+  }else if (op=== '%' || op==='modulus' || op==='mod') {
+    return num1 % num2
+  
+  }else {
+  return "Sorry, that's not a mathematical operation!"
+  }
+  
+  
+  }
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
